110b2eff465248ba8f9dbf0df3e71e4a83ba8e44
Brian Dillingham <bdillingham88@gmail.com>
2019-01-11 14:34:37 -0500

    add error display

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,5 +1,5 @@
 <template>
-    <default-field :field="field" :errors="errors" :full-width-content="field.fullWidth">
+    <default-field :field="field" :full-width-content="field.fullWidth">
         <template slot="field" class="nova-items-field">
             <div class="nova-items-field-input-wrapper flex border border-40 p-4" v-if="field.listFirst == false">
                 <input 
@@ -10,13 +10,18 @@
                 />
                 <a @click="addItem" class="btn btn-default btn-primary ml-3 cursor-pointer" v-html="field.createButtonValue"/>
             </div>
-            <ul v-if="items.length" :id="field.name" class="nova-items-field-input-items list-reset border border-40 p-4 pb-0">
-                <li v-for="(item, index) in items" :key="index" :class="errorClasses" class="nova-items-field-input-wrapper-item flex py-1 pb-4">
+            <ul v-if="items.length" :id="field.attribute" class="nova-items-field-input-items list-reset border border-40 p-4 pb-0">
+                <li 
+                    v-for="(item, index) in items" 
+                    :key="field.attribute + '.' + index" 
+                >
+                    <div class="nova-items-field-input-wrapper-item flex py-1 pb-4">
                     <input 
                         :value="item" 
                         :type="field.inputType"
                         v-on:keyup="updateItem(index, $event)"
                         :name="field.name + '['+ index +']'" 
+                        :class="{'border-danger': hasErrors(field.attribute + '.' + index)}"
                         class="flex-1 form-control form-input form-input-bordered"
                     >
                     <span 
@@ -24,6 +29,10 @@
                         class="ml-4 mr-2 font-thin text-2xl cursor-pointer hover:font-normal"
                         v-html="field.deleteButtonValue"
                     />
+                    </div>
+                    <div v-if="hasErrors(field.attribute + '.' + index)" class="help-text error-text -mt-2 text-danger pb-2">
+                        <p v-html="arrayErrors[field.attribute + '.' + index][0]" />
+                    </div>
                 </li>
             </ul>
              <div class="nova-items-field-input-wrapper flex border border-40 p-4"  v-if="field.listFirst">
@@ -51,7 +60,8 @@ export default {
         return {
             value: '',
             items: [],
-            newItem: ''
+            newItem: '',
+            arrayErrors: []
         }
     },
 
@@ -89,12 +99,26 @@ export default {
 
 		removeItem (index) {
 			this.items.splice(index, 1)
+        },
+    
+        hasErrors(key)
+        {
+            return this.arrayErrors.hasOwnProperty(key);
 		}
     },
     watch: {
         'items': {
             handler: function (items) {
                 this.value = JSON.stringify(items);
+            },
+            deep: true
+        },
+        'errors': {
+            handler: function (errors) {
+                if(errors.errors.hasOwnProperty(this.field.attribute))
+                {
+                    this.arrayErrors = errors.errors[this.field.attribute][0]
+                }
             },
             deep: true
         }
