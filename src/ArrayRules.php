3df2eb02cfe55df89218f94601ed862c76ae97aa
Brian Dillingham <bdillingham88@gmail.com>
2019-01-11 14:34:48 -0500

    add custom array handling

---
+++ src/ArrayRules.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NovaItemsField;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class ArrayRules implements Rule
+{
+    public $rules = [];
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct(array $rules)
+    {
+        $this->rules = $rules;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $input = [$attribute => json_decode($value)];
+
+        $validator = \Validator::make($input, $this->rules, [], [ "$attribute.*" => 'input']);
+
+        $this->message = $validator->errors();
+
+        return $validator->passes();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return $this->message;
+    }
+}
