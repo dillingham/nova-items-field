bc3c5fa337ef147e83bdf5a62a930eb59f5ca028
Daniel Schmitz <daniel@blendbyte.com>
2022-04-10 15:23:35 +0800

    wip

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,134 +1,107 @@
 <template>
-    <default-field :field="field" :full-width-content="field.fullWidth" :show-help-text="showHelpText">
-        <template slot="field" class="nova-items-field">
-            <div class="nova-items-field-input-wrapper flex border border-40 p-4" v-if="field.listFirst == false && ! maxReached">
+    <DefaultField :field="currentField" :full-width-content="currentField.fullWidth" :show-help-text="showHelpText">
+        <template #field>
+            <div class="nova-items-field-input-wrapper flex border border-40 p-4" v-if="currentField.listFirst === false && ! maxReached">
                 <input
                     v-model="newItem"
-                    :type="field.inputType"
-                    :placeholder="field.placeholder"
+                    :type="currentField.inputType"
+                    :placeholder="currentField.placeholder"
                     autocomplete="new-password"
                     @keydown.enter.prevent="addItem"
                     class="flex-1 form-control form-input form-input-bordered"
                 />
-                <a
+                <button
+                    type="button"
                     @click="addItem"
-                    v-html="field.createButtonValue"
-                    v-if="field.hideCreateButton == false"
-                    class="btn btn-default btn-primary ml-3 cursor-pointer font-sans"
+                    v-html="currentField.createButtonValue"
+                    v-if="currentField.hideCreateButton === false"
+                    class="ml-3 cursor-pointer shadow relative bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-900 cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring inline-flex items-center justify-center h-9 px-3 shadow relative bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-900"
                 />
             </div>
             <ul ref="novaitemslist" :style="maxHeight" v-if="items.length" class="nova-items-field-input-items list-reset border border-40 py-2">
-                <draggable v-model="items" :options="{ disabled: field.draggable == false, handle: '.sortable-handle' }">
-                <li
-                    v-for="(item, index) in items"
-                    :key="field.attribute + '.' + index"
-                    class="px-4 py-2"
-                >
-
-                    <div class="nova-items-field-input-wrapper-item flex py-1">
-                        <span v-if="field.draggable" class="sortable-handle py-2 pl-0 pr-4 text-80 cursor-move">
-                            |||
-                        </span>
-                        <input
-                            :value="item"
-                            :type="field.inputType"
-                            v-on:keyup="updateItem(index, $event)"
-                            :name="field.name + '['+ index +']'"
-                            autocomplete="new-password"
-                            :class="{'border-danger': hasErrors(field.attribute + '.' + index)}"
-                            class="flex-1 form-control form-input form-input-bordered"
-                        >
-                        <span
-                            @click="removeItem(index)"
-                            style="font-size: 32px;"
-                            class="ml-4 mr-2 font-thin cursor-pointer hover:font-normal"
-                            v-html="field.deleteButtonValue"
-                        />
-                        </div>
-                        <div v-if="hasErrors(field.attribute + '.' + index)" class="help-text error-text -mt-2 text-danger py-2">
-                            <p v-html="arrayErrors[field.attribute + '.' + index][0]" />
-                        </div>
-                    </li>
+                <draggable v-model="items" :item-key="currentField.attribute + '.' + index" :options="{ disabled: currentField.draggable === false, handle: '.sortable-handle' }">
+                    <template #item="{ element }">
+                        <li class="py-2">
+                            <div class="nova-items-field-input-wrapper-item flex py-1 gap-2">
+                                <button type="button" class="sortable-handle px-4"><Icon type="view-list" /></button>
+                                <input
+                                    :value="element"
+                                    :type="currentField.inputType"
+                                    v-on:keyup="updateItem(index, $event)"
+                                    :name="currentField.name + '['+ index +']'"
+                                    autocomplete="new-password"
+                                    :class="{'border-danger': hasErrors(currentField.attribute + '.' + index)}"
+                                    class="flex-1 form-control form-input form-input-bordered"
+                                >
+                                <button
+                                    type="button"
+                                    @click="removeItem(index)"
+                                    class="px-4 text-xl font-bold focus:outline-none focus:ring"
+                                    v-html="currentField.deleteButtonValue"
+                                />
+                            </div>
+                            <div v-if="hasErrors(currentField.attribute + '.' + index)" class="help-text error-text -mt-2 text-danger py-2">
+                                <p v-html="arrayErrors[currentField.attribute + '.' + index][0]" />
+                            </div>
+                        </li>
+                    </template>
                 </draggable>
             </ul>
-             <div class="nova-items-field-input-wrapper flex border border-40 p-4"  v-if="field.listFirst && ! maxReached">
+             <div class="nova-items-field-input-wrapper flex border border-40 p-4"  v-if="currentField.listFirst && ! maxReached">
                 <input
                     v-model="newItem"
-                    :type="field.inputType"
-                    :placeholder="field.placeholder"
+                    :type="currentField.inputType"
+                    :placeholder="currentField.placeholder"
                     class="flex-1 form-control form-input form-input-bordered"
                     @keypress.enter.prevent="addItem"
                 />
-                <a
-                    @click="addItem"
-                    v-html="field.createButtonValue"
-                    v-if="field.hideCreateButton == false"
-                    class="btn btn-default btn-primary ml-3 cursor-pointer"
-                />
+                 <button
+                     type="button"
+                     @click="addItem"
+                     v-html="currentField.createButtonValue"
+                     v-if="currentField.hideCreateButton === false"
+                     class="ml-3 cursor-pointer shadow relative bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-900 cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring inline-flex items-center justify-center h-9 px-3 shadow relative bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-900"
+                 />
             </div>
         </template>
-    </default-field>
+    </DefaultField>
 </template>
 
 <style scoped>
-    .sortable-chosen {
-        border: 1px solid var(--50);
-        background-color: var(--20);
-        box-shadow: 2px 2px 2px var(--40);
-        margin-left: -5px;
-    }
-    .sortable-handle {
-        -o-user-select: none;
-        -moz-user-select: none;
-        -webkit-user-select: none;
-        user-select: none;
-        -webkit-transform: rotate(90deg);
-        -moz-transform: rotate(90deg);
-        -o-transform: rotate(90deg);
-        -ms-transform: rotate(90deg);
-        transform: rotate(90deg);
-        position: relative;
-        left: -5px;
-        top: 5px;
-    }
 </style>
 
 <script>
 import draggable from 'vuedraggable'
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
+import { DependentFormField, HandlesValidationErrors } from 'laravel-nova'
 
 export default {
-    mixins: [FormField, HandlesValidationErrors],
+    mixins: [DependentFormField, HandlesValidationErrors],
 
     props: ['resourceName', 'resourceId', 'field'],
 
     components: { draggable },
 
-    data: function() {
+    data() {
         return {
             value: '',
             items: [],
             newItem: '',
             arrayErrors: []
-        }
+        };
     },
 
     methods: {
-
-
         setInitialValue() {
             this.value = this.field.value || [];
             this.items = this.field.value || [];
         },
-
 
         fill(formData) {
             formData.append(this.field.attribute, this.value || [])
         },
 
 		addItem() {
-            const item = this.newItem.trim()
-
+            const item = this.newItem.trim();
 			if (item && ! this.maxReached) {
 				this.items.push(item)
                 this.newItem = ''
@@ -141,8 +114,7 @@ export default {
 			}
         },
 
-        updateItem(index, event)
-        {
+        updateItem(index, event) {
             this.$set(this.items, index, event.target.value)
         },
 
@@ -150,22 +122,19 @@ export default {
 			this.items.splice(index, 1)
         },
 
-        hasErrors(key)
-        {
+        hasErrors(key) {
             return this.arrayErrors.hasOwnProperty(key);
 		}
     },
     computed: {
-        maxHeight()
-        {
-            if(this.field.maxHeight == false) {
+        maxHeight() {
+            if (this.field.maxHeight === false) {
                 return '';
             }
 
             return `max-height: ${this.field.maxHeight}px; overflow: auto;`;
         },
-        maxReached()
-        {
+        maxReached() {
             return this.field.max !== false && this.items.length + 1 > this.field.max;
         }
     },
@@ -178,8 +147,7 @@ export default {
         },
         'errors': {
             handler: function (errors) {
-                if(errors.errors.hasOwnProperty(this.field.attribute))
-                {
+                if(errors.errors.hasOwnProperty(this.field.attribute)) {
                     this.arrayErrors = errors.errors[this.field.attribute][0]
                 }
             },
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,10 @@
 let mix = require('laravel-mix')
+let path = require('path')
 
-mix.setPublicPath('dist').js('resources/js/field.js', 'js')
+require('./mix')
+
+mix
+    .setPublicPath('dist')
+    .js('resources/js/field.js', 'js')
+    .vue({ version: 3 })
+    .nova('dillingham/nova-items-field')
